22b852ce7cf572bfef960cf890e03a671eb58c2b
compliance-user <63388297+compliance-user@users.noreply.github.com>
2022-12-01 12:58:31 +0530

    Create azure_audit_security_contact_email.py

---
+++ cs_policy_interface/cs_policy_interface/rules/azure_audit_security_contact_email.py
@@ -0,0 +1,35 @@
+import requests
+
+from collections import OrderedDict
+from cs_policy_interface.utils import get_azure_auth_token
+from cs_policy_interface.definitions import AzureRestApiEndpoint, AzureRequestHeader
+
+#"This policy audits whether Security contact Email has been set."
+
+class RuleExecutor(object):
+    def __init__(self, execution_args, connection_args):
+        self.execution_args = execution_args
+        self.connection_args = connection_args
+
+    def execute(self, **kwargs):
+        output = list()
+        evaluated_resources = 0
+        try:
+            credentials = self.execution_args['auth_values']
+            bearer_token, endpoint = get_azure_auth_token(credentials)
+            resource_url = AzureRestApiEndpoint.list_security_contacts.format(endpoint, credentials['subscription_id'])
+            headers = AzureRequestHeader.header.update(Authorization="Bearer {}".format(bearer_token))
+            get_response = requests.get(resource_url, headers=headers)
+            for each_resource in get_response.json():
+                evaluated_resources += 1
+                if each_resource.get('properties', {}).get('alertNotifications', {}).get('state', {}) == "On":
+                    if not each_resource.get('emails', {}):
+                        output.append(OrderedDict(ResourceId=each_resource.get('id'),
+                                                  ResourceName=each_resource.get('name'),
+                                                  Resource="Security_contact",
+                                                  ResourceType='Azure_Security_Center',
+                                                  ResourceCategory='Security_Compliance'
+                                                  ))
+            return output, evaluated_resources
+        except Exception as e:
+            raise Exception(str(e))
